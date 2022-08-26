f5400f10c76ef124c25429a59e647f5e775fee01
Kevin Palacino <lunagonzalez688@gmal.com>
2022-08-26 11:55:01 -0500

    rutas-rest

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BootcampController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//  return $request->user();
+//});
+
+//Clase Route: Encragado de rutas 
+//metodo apiResource: Crear ritas de cambios de estado
+//para una entidad
+Route::apiResource('bootcamps', BootcampController::class);
+
+
